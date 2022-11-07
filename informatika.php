e73c550af6288bb3cecd172658a553f6f2386468
redmain <i@gmail.com>
2022-11-07 13:18:34 +0800

    4233223

---
+++ informatika.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Информатика</title>
+</head>
+<body>
+    <h1>Информатика</h1>
+    <p>Ну я уже не знаю что можно здесь запихать</p>
+</body>
+</html>
---
+++ russian.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Russian</title>
+</head>
+<body>
+    <h1>я РУССКИЙ</h1>
+    <p>    Тоже самое, только для русского</p>
+</body>
+</html>
